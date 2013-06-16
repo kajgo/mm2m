8b76abc50015b34c1fde4dc0797da8daeb5cc85b
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2013-06-16 17:30:06 +0200

    Test populates db.

--- test.bash
+++ test.bash
@@ -1,13 +1,32 @@
 assertPageContains() {
-if (curl -s http://localhost:8000$1 | grep "$2" > /dev/null); then
-    echo "pass"
-else
-    echo "fail"
-fi
+    if (curl -s http://localhost:8000$1 | grep "$2" > /dev/null); then
+        echo "pass"
+    else
+        echo "fail"
+    fi
 }
+
+dropVisitors() {
+    mongo --eval "db.visitors.drop()" > /dev/null
+}
+
+addVisitor() {
+    mongo --eval "db.visitors.insert({})" > /dev/null
+}
+
+testVisitorCountShowUpOnPage() {
+    dropVisitors
+    addVisitor
+    addVisitor
+    addVisitor
+    assertPageContains "/" "3"
+}
+
 ghc --make Main.hs || exit
 ./Main &
 serverPid=$!
 sleep 1
-assertPageContains "/" "3"
+
+testVisitorCountShowUpOnPage
+
 kill -9 $serverPid
