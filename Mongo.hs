01a364582536a48723ed835b6eca12c3b3820751
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2013-06-16 18:07:46 +0200

    Refactor.

--- Mongo.hs
+++ Mongo.hs
@@ -6,27 +6,22 @@ module Mongo where
 import Database.MongoDB
 import Prelude hiding (lookup)
 
-registerVisitor :: IO ()
-registerVisitor = do
-    pipe <- runIOE $ connect (host "127.0.0.1")
-    e <- access pipe master "test" run
-    close pipe
-    return ()
-    where
-        run :: Action IO ()
-        run = do
-            let doc = []
-            insert_ "visitors" doc
-
 getVisitors :: IO (Either Failure Int)
-getVisitors = do
+getVisitors = withTestDb $ do
+    cursor <- find (select [] visitorsCollection)
+    docs <- rest cursor
+    return $ length docs
+
+registerVisitor :: IO (Either Failure ())
+registerVisitor = withTestDb $ do
+    let doc = []
+    insert_ visitorsCollection doc
+
+withTestDb :: (Action IO a) -> IO (Either Failure a)
+withTestDb run = do
     pipe <- runIOE $ connect (host "127.0.0.1")
     e <- access pipe master "test" run
     close pipe
     return e
-    where
-        run :: Action IO Int
-        run = do
-            cursor <- find (select [] "visitors")
-            docs <- rest cursor
-            return $ length docs
+
+visitorsCollection = "visitors"
